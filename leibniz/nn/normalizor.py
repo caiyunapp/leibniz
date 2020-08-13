a11d4c0707dddc377b4e50189a4ce0f9653b4c87
mingli <mingli.yuan@gmail.com>
2020-08-13 18:01:27 +0800

    bump t0 0.0.24

--- leibniz/nn/normalizor.py
+++ leibniz/nn/normalizor.py
@@ -1,6 +1,7 @@
+import torch as th
 import torch.nn as nn
 
-from torchpwl.pwl import BasePointPWL, MonoPointPWL, MonoSlopedPWL
+from torchpwl.pwl import BasePointPWL, MonoPointPWL
 
 
 class InversedMonoPointPWL(BasePointPWL):
@@ -12,21 +13,26 @@ class InversedMonoPointPWL(BasePointPWL):
         self.peer._reset_params()
 
     def get_y_positions(self):
-        return self.peer.get_sorted_x_positions()
+        return th.sort(self.peer.get_x_positions(), dim=1, descending=self.peer.mono<0)[0]
 
     def get_x_positions(self):
-        xs = self.peer.get_sorted_x_positions()
-        data = xs.transpose(1, 0)
-        return self.peer(data).transpose(1, 0)
+        return th.sort(self.peer.get_y_positions(), dim=1, descending=self.peer.mono<0)[0]
 
 
 class PWLNormalizorApp(nn.Module):
     def __init__(self, num_channels, num_breakpoints, monotonicity=1):
         super(PWLNormalizorApp, self).__init__()
         self.pwl = MonoPointPWL(num_channels, num_breakpoints, monotonicity=monotonicity)
+        self.pwl.mono = monotonicity
 
     def forward(self, x):
-        return self.pwl(x)
+        sz1 = list(x.size())
+        l, c = len(sz1), sz1[1]
+        shift = [0] + list(range(2, l)) + [1]
+        unshift = [0, l - 1] + list(range(1, l - 1))
+        sz2 = [sz1[0]] + list([sz1[i] for i in range(2, l)]) + [sz1[1]]
+        x = x.permute(*shift).reshape(-1, c)
+        return self.pwl(x).reshape(*sz2).permute(*unshift).reshape(*sz1)
 
 
 class PWLNormalizorInv(nn.Module):
@@ -35,19 +41,25 @@ class PWLNormalizorInv(nn.Module):
         self.pwl = InversedMonoPointPWL(app.pwl)
 
     def forward(self, x):
-        return self.pwl(x)
+        sz1 = list(x.size())
+        l, c = len(sz1), sz1[1]
+        shift = [0] + list(range(2, l)) + [1]
+        unshift = [0, l - 1] + list(range(1, l - 1))
+        sz2 = [sz1[0]] + list([sz1[i] for i in range(2, l)]) + [sz1[1]]
+        x = x.permute(*shift).reshape(-1, c)
+        return self.pwl(x).reshape(*sz2).permute(*unshift).reshape(*sz1)
 
 
 class PWLNormalizor(nn.Module):
-    def __init__(self, num_channels, num_breakpoints, monotonicity=1):
+    def __init__(self, num_channels, num_breakpoints, monotonicity=1, std=1.0, mean=0.0):
         super(PWLNormalizor, self).__init__()
         self.app = PWLNormalizorApp(num_channels, num_breakpoints, monotonicity=monotonicity)
         self.inv = PWLNormalizorInv(self.app)
+        self.mean = mean
+        self.std = std
 
     def forward(self, x):
-        return self.app(x)
+        return self.app((x - self.mean) / self.std)
 
     def inverse(self, x):
-        return self.inv(x)
-
-
+        return self.inv(x) * self.std + self.mean
--- tests/test_normalizor.py
+++ tests/test_normalizor.py
@@ -14,26 +14,16 @@ class TestNormalizor(unittest.TestCase):
     def setUp(self):
         self.tanh = Tanh()
         self.atanh = Atanh()
-        self.pwln = PWLNormalizor(2, 128)
-        self.mlp = Sequential(
-            self.pwln.app,
-            nn.Linear(2, 16),
-            nn.ReLU(inplace=True),
-            nn.Linear(16, 16),
-            nn.ReLU(inplace=True),
-            nn.Linear(16, 2),
-            self.pwln.inv,
-        )
-        self.apx = Sequential(
-            self.pwln.app,
-            self.pwln.inv,
-        )
+        self.pwln = PWLNormalizor(2, 16, mean=1.4, std=2.5)
 
     def tearDown(self):
         pass
 
     def func(self, x):
-        return x * 2 + 0.5 * th.cos(x * 8)
+        return th.cat([
+            x[:, 0:1] * 2 + 0.5 * th.cos(x[:, 0:1] * 8),
+            x[:, 1:2] * 3 - 0.5 * th.sin(x[:, 1:2] * 7),
+        ], dim=1)
 
     def testAtanh(self):
         self.assertAlmostEqual(6.0, self.atanh(self.tanh(th.as_tensor(6.0))).numpy(), places=3)
@@ -60,29 +50,111 @@ class TestNormalizor(unittest.TestCase):
         self.assertAlmostEqual(0.8, self.atanh(self.tanh(th.as_tensor(0.8))).numpy())
         self.assertAlmostEqual(0.9, self.atanh(self.tanh(th.as_tensor(0.9))).numpy())
 
-    def testPWLN(self):
-        lr = 0.001
-        optimizer = th.optim.Adam(self.mlp.parameters(), lr=lr)
+    def testPWLN0D(self):
+        lr = 0.01
+        mlp = Sequential(
+            self.pwln.app,
+            nn.Linear(2, 16),
+            nn.ReLU(inplace=True),
+            nn.Linear(16, 16),
+            nn.ReLU(inplace=True),
+            nn.Linear(16, 2),
+            self.pwln.inv,
+        )
+        optimizer = th.optim.Adam(mlp.parameters(), lr=lr)
         mse = MSELoss()
         for _ in range(10):
-            for _ in range(100):
-                data = self.func(th.rand(128, 2))
-                loss = mse(self.mlp(data), data * data * data)
+            for _ in range(10):
+                data = self.func(th.rand(16, 2))
+                loss = mse(mlp(data), data * data * data)
                 print('loss', loss.item())
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
 
             with th.no_grad():
-                print(0.1, self.func(th.as_tensor([0.1, 0.1])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.1, 0.1]).reshape(1, 2)))).detach().numpy())
-                print(0.2, self.func(th.as_tensor([0.2, 0.2])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.2, 0.2]).reshape(1, 2)))).detach().numpy())
-                print(0.3, self.func(th.as_tensor([0.3, 0.3])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.3, 0.3]).reshape(1, 2)))).detach().numpy())
-                print(0.4, self.func(th.as_tensor([0.4, 0.4])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.4, 0.4]).reshape(1, 2)))).detach().numpy())
-                print(0.5, self.func(th.as_tensor([0.5, 0.5])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.5, 0.5]).reshape(1, 2)))).detach().numpy())
-                print(0.6, self.func(th.as_tensor([0.6, 0.6])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.6, 0.6]).reshape(1, 2)))).detach().numpy())
-                print(0.7, self.func(th.as_tensor([0.7, 0.7])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.7, 0.7]).reshape(1, 2)))).detach().numpy())
-                print(0.8, self.func(th.as_tensor([0.8, 0.8])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.8, 0.8]).reshape(1, 2)))).detach().numpy())
-                print(0.9, self.func(th.as_tensor([0.9, 0.9])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.9, 0.9]).reshape(1, 2)))).detach().numpy())
+                print(0.1, self.func(th.as_tensor([[0.1, 0.1]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.1, 0.1]).reshape(1, 2)))).detach().numpy())
+                print(0.2, self.func(th.as_tensor([[0.2, 0.2]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.2, 0.2]).reshape(1, 2)))).detach().numpy())
+                print(0.3, self.func(th.as_tensor([[0.3, 0.3]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.3, 0.3]).reshape(1, 2)))).detach().numpy())
+                print(0.4, self.func(th.as_tensor([[0.4, 0.4]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.4, 0.4]).reshape(1, 2)))).detach().numpy())
+                print(0.5, self.func(th.as_tensor([[0.5, 0.5]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.5, 0.5]).reshape(1, 2)))).detach().numpy())
+                print(0.6, self.func(th.as_tensor([[0.6, 0.6]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.6, 0.6]).reshape(1, 2)))).detach().numpy())
+                print(0.7, self.func(th.as_tensor([[0.7, 0.7]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.7, 0.7]).reshape(1, 2)))).detach().numpy())
+                print(0.8, self.func(th.as_tensor([[0.8, 0.8]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.8, 0.8]).reshape(1, 2)))).detach().numpy())
+                print(0.9, self.func(th.as_tensor([[0.9, 0.9]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.9, 0.9]).reshape(1, 2)))).detach().numpy())
+
+        data = self.func(th.rand(1, 2))
+        self.assertAlmostEqual(data.detach().numpy()[0, 0], self.pwln.inverse(self.pwln(data)).detach().numpy()[0, 0], places=3)
+
+    def testPWLN1D(self):
+        lr = 0.01
+        mlp = Sequential(
+            self.pwln.app,
+            nn.Conv1d(2, 16, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(16, 16, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(16, 2, kernel_size=3, padding=1),
+            self.pwln.inv,
+        )
+        optimizer = th.optim.Adam(mlp.parameters(), lr=lr)
+        mse = MSELoss()
+        for _ in range(10):
+            for _ in range(10):
+                data = self.func(th.rand(16, 2, 16))
+                loss = mse(mlp(data), data * data * data)
+                print('loss', loss.item())
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            with th.no_grad():
+                print(0.1, self.func(th.as_tensor([[0.1, 0.1]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.1, 0.1]).reshape(1, 2)))).detach().numpy())
+                print(0.2, self.func(th.as_tensor([[0.2, 0.2]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.2, 0.2]).reshape(1, 2)))).detach().numpy())
+                print(0.3, self.func(th.as_tensor([[0.3, 0.3]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.3, 0.3]).reshape(1, 2)))).detach().numpy())
+                print(0.4, self.func(th.as_tensor([[0.4, 0.4]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.4, 0.4]).reshape(1, 2)))).detach().numpy())
+                print(0.5, self.func(th.as_tensor([[0.5, 0.5]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.5, 0.5]).reshape(1, 2)))).detach().numpy())
+                print(0.6, self.func(th.as_tensor([[0.6, 0.6]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.6, 0.6]).reshape(1, 2)))).detach().numpy())
+                print(0.7, self.func(th.as_tensor([[0.7, 0.7]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.7, 0.7]).reshape(1, 2)))).detach().numpy())
+                print(0.8, self.func(th.as_tensor([[0.8, 0.8]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.8, 0.8]).reshape(1, 2)))).detach().numpy())
+                print(0.9, self.func(th.as_tensor([[0.9, 0.9]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.9, 0.9]).reshape(1, 2)))).detach().numpy())
+
+        data = self.func(th.rand(1, 2))
+        self.assertAlmostEqual(data.detach().numpy()[0, 0], self.pwln.inverse(self.pwln(data)).detach().numpy()[0, 0], places=3)
+
+
+    def testPWLN2D(self):
+        lr = 0.01
+        mlp = Sequential(
+            self.pwln.app,
+            nn.Conv2d(2, 16, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(16, 16, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(16, 2, kernel_size=3, padding=1),
+            self.pwln.inv,
+        )
+        optimizer = th.optim.Adam(mlp.parameters(), lr=lr)
+        mse = MSELoss()
+        for _ in range(10):
+            for _ in range(10):
+                data = self.func(th.rand(16, 2, 16, 16))
+                loss = mse(mlp(data), data * data * data)
+                print('loss', loss.item())
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            with th.no_grad():
+                print(0.1, self.func(th.as_tensor([[0.1, 0.1]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.1, 0.1]).reshape(1, 2)))).detach().numpy())
+                print(0.2, self.func(th.as_tensor([[0.2, 0.2]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.2, 0.2]).reshape(1, 2)))).detach().numpy())
+                print(0.3, self.func(th.as_tensor([[0.3, 0.3]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.3, 0.3]).reshape(1, 2)))).detach().numpy())
+                print(0.4, self.func(th.as_tensor([[0.4, 0.4]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.4, 0.4]).reshape(1, 2)))).detach().numpy())
+                print(0.5, self.func(th.as_tensor([[0.5, 0.5]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.5, 0.5]).reshape(1, 2)))).detach().numpy())
+                print(0.6, self.func(th.as_tensor([[0.6, 0.6]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.6, 0.6]).reshape(1, 2)))).detach().numpy())
+                print(0.7, self.func(th.as_tensor([[0.7, 0.7]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.7, 0.7]).reshape(1, 2)))).detach().numpy())
+                print(0.8, self.func(th.as_tensor([[0.8, 0.8]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.8, 0.8]).reshape(1, 2)))).detach().numpy())
+                print(0.9, self.func(th.as_tensor([[0.9, 0.9]])).detach().numpy(), self.pwln.inverse(self.pwln(self.func(th.as_tensor([0.9, 0.9]).reshape(1, 2)))).detach().numpy())
 
         data = self.func(th.rand(1, 2))
         self.assertAlmostEqual(data.detach().numpy()[0, 0], self.pwln.inverse(self.pwln(data)).detach().numpy()[0, 0], places=3)
