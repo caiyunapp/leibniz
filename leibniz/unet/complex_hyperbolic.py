bed656c26d6f344ed2c08311d6c0422a901d0be7
mingli <mingli.yuan@gmail.com>
2020-08-24 16:28:04 +0800

    bump t0 0.0.31: change basic to a simpler form

--- leibniz/unet/complex_hyperbolic.py
+++ leibniz/unet/complex_hyperbolic.py
@@ -8,7 +8,7 @@ from leibniz.unet.senet import SELayer
 
 
 class CmplxHyperBasic(nn.Module):
-    extension = 3
+    extension = 2
     least_required_dim = 1
 
     def __init__(self, dim, step, relu, conv, reduction=16):
@@ -27,33 +27,28 @@ class CmplxHyperBasic(nn.Module):
             self.conv = conv
 
         self.conv0 = self.conv(dim, dim, kernel_size=3, padding=1)
-        self.conv1 = self.conv(dim, dim // 3, kernel_size=3, padding=1)
-        self.conv2 = self.conv(10 * dim // 3, 10 * dim // 3, kernel_size=3, padding=1)
-        self.conv3 = self.conv(10 * dim // 3, dim, kernel_size=3, padding=1)
+        self.conv1 = self.conv(dim, dim // 2, kernel_size=3, padding=1)
+        self.conv2 = self.conv(dim, dim, kernel_size=3, padding=1)
+        self.conv3 = self.conv(dim, dim // 2, kernel_size=3, padding=1)
         self.se = SELayer(dim, reduction)
 
     def forward(self, z):
-        x, y, theta = z[:, 0:self.dim // 3], z[:, self.dim // 3:self.dim * 2 // 3], z[:, self.dim * 2 // 3:self.dim]
+        x, y = z[:, 0:self.dim // 2], z[:, self.dim // 2:self.dim]
+
         u = self.conv0(z)
         u = self.relu(u)
         u = self.conv1(u)
 
+        theta = self.conv2(z)
+        theta = self.relu(theta)
+        theta = self.conv3(theta)
+
         cs = self.step * u * th.cos(theta * np.pi * 6)
         ss = self.step * u * th.sin(theta * np.pi * 6)
 
-        x1 = (1 + ss) * x - cs * y + cs
-        y1 = (1 + ss) * y + cs * x + ss
-        x2 = (1 + cs) * x + ss * y - ss
-        y2 = (1 + cs) * y - ss * x + cs
-        x3 = (1 - ss) * x + cs * y - cs
-        y3 = (1 - ss) * y - cs * x - ss
-        x4 = (1 - cs) * x - ss * y + ss
-        y4 = (1 - cs) * y + ss * x - cs
-        zs = th.cat((x1, y1, x2, y2, x3, y3, x4, y4, cs, ss), dim=1)
-
-        dz = self.conv2(zs)
-        dz = self.relu(dz)
-        dz = self.conv3(dz)
+        dx = ss * x - cs * y + cs
+        dy = ss * y + cs * x + ss
+        dz = th.cat([dx, dy], dim=1)
 
         z = z + self.se(dz)
 
