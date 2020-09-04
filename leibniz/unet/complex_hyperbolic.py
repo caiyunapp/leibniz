84e6007f010fb21bb4e678e934c0a82d4b226a2b
mingli <mingli.yuan@gmail.com>
2020-09-04 13:11:17 +0800

    bump t0 0.0.45: fix complex_hyperbolic

--- leibniz/unet/complex_hyperbolic.py
+++ leibniz/unet/complex_hyperbolic.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch as th
 import torch.nn as nn
 
-from leibniz.unet.senet import SELayer
+from leibniz.unet.hyperbolic import BasicBlock, Bottleneck
 
 
 class CmplxHyperBasic(nn.Module):
@@ -16,79 +16,18 @@ class CmplxHyperBasic(nn.Module):
         self.dim = dim
         self.step = step
 
-        if relu is None:
-            self.relu = nn.ReLU(inplace=True)
-        else:
-            self.relu = relu
-
-        if conv is None:
-            self.conv = nn.Conv2d
-        else:
-            self.conv = conv
-
-        self.conv0 = self.conv(dim, dim, kernel_size=3, padding=1, bias=False)
-        self.conv1 = self.conv(dim, dim // 2, kernel_size=3, padding=1, bias=False)
-        self.conv2 = self.conv(dim, dim, kernel_size=3, padding=1, bias=False)
-        self.conv3 = self.conv(dim, dim // 2, kernel_size=3, padding=1, bias=False)
-        self.se = SELayer(dim, reduction)
+        self.input = BasicBlock(dim, 4 * dim, step, relu, conv, reduction=reduction)
+        self.output = BasicBlock(8 * dim, dim, step, relu, conv, reduction=reduction)
 
     def forward(self, z):
-        x, y = z[:, 0:self.dim // 2], z[:, self.dim // 2:self.dim]
+        input = self.input(z)
+        x = input[:, 0 * self.dim:1 * self.dim]
+        y = input[:, 1 * self.dim:2 * self.dim]
+        v = input[:, 2 * self.dim:3 * self.dim]
+        t = input[:, 3 * self.dim:4 * self.dim]
 
-        u = self.conv0(z)
-        u = self.relu(u)
-        u = self.conv1(u)
-
-        theta = self.conv2(z)
-        theta = self.relu(theta)
-        theta = self.conv3(theta)
-
-        cs = self.step * u * th.cos(theta * np.pi * 6)
-        ss = self.step * u * th.sin(theta * np.pi * 6)
-
-        dx = ss * x - cs * y + cs
-        dy = ss * y + cs * x + ss
-        dz = th.cat([dx, dy], dim=1)
-
-        z = z + self.se(dz)
-
-        return z
-
-
-class CmplxHyperBottleneck(nn.Module):
-    extension = 6
-    least_required_dim = 1
-
-    def __init__(self, dim, step, relu, conv, reduction=16):
-        super(CmplxHyperBottleneck, self).__init__()
-        self.dim = dim
-        self.step = step
-
-        if relu is None:
-            self.relu = nn.ReLU(inplace=True)
-        else:
-            self.relu = relu
-
-        if conv is None:
-            self.conv = nn.Conv2d
-        else:
-            self.conv = conv
-
-        self.conv0 = self.conv(dim, dim, kernel_size=3, padding=1)
-        self.conv1 = self.conv(dim, dim // 3, kernel_size=3, padding=1)
-        self.conv2 = self.conv(10 * dim // 3, 10 * dim // 12, kernel_size=1, bias=False)
-        self.conv3 = self.conv(10 * dim // 12, dim // 3, kernel_size=3, bias=False, padding=1)
-        self.conv4 = self.conv(dim // 3, dim, kernel_size=1, bias=False)
-        self.se = SELayer(dim, reduction)
-
-    def forward(self, z):
-        x, y, theta = z[:, 0:self.dim // 3], z[:, self.dim // 3:self.dim * 2 // 3], z[:, self.dim * 2 // 3:self.dim]
-        u = self.conv0(z)
-        u = self.relu(u)
-        u = self.conv1(u)
-
-        cs = self.step * u * th.cos(theta * np.pi * 6)
-        ss = self.step * u * th.sin(theta * np.pi * 6)
+        cs = self.step * v * th.cos(t * np.pi * 6)
+        ss = self.step * v * th.sin(t * np.pi * 6)
 
         x1 = (1 + ss) * x - cs * y + cs
         y1 = (1 + ss) * y + cs * x + ss
@@ -98,14 +37,41 @@ class CmplxHyperBottleneck(nn.Module):
         y3 = (1 - ss) * y - cs * x - ss
         x4 = (1 - cs) * x - ss * y + ss
         y4 = (1 - cs) * y + ss * x - cs
-        zs = th.cat((x1, y1, x2, y2, x3, y3, x4, y4, cs, ss), dim=1)
+        zs = th.cat((x1, y1, x2, y2, x3, y3, x4, y4), dim=1)
 
-        dz = self.conv2(zs)
-        dz = self.relu(dz)
-        dz = self.conv3(dz)
-        dz = self.relu(dz)
-        dz = self.conv4(dz)
+        return z + self.output(zs)
 
-        z = z + self.se(dz)
 
-        return z
+class CmplxHyperBottleneck(nn.Module):
+    extension = 4
+    least_required_dim = 1
+
+    def __init__(self, dim, step, relu, conv, reduction=16):
+        super(CmplxHyperBottleneck, self).__init__()
+        self.dim = dim
+        self.step = step
+
+        self.input = Bottleneck(dim, 4 * dim, step, relu, conv, reduction=reduction)
+        self.output = Bottleneck(8 * dim, dim, step, relu, conv, reduction=reduction)
+
+    def forward(self, z):
+        input = self.input(z)
+        x = input[:, 0 * self.dim:1 * self.dim]
+        y = input[:, 1 * self.dim:2 * self.dim]
+        v = input[:, 2 * self.dim:3 * self.dim]
+        t = input[:, 3 * self.dim:4 * self.dim]
+
+        cs = self.step * v * th.cos(t * np.pi * 6)
+        ss = self.step * v * th.sin(t * np.pi * 6)
+
+        x1 = (1 + ss) * x - cs * y + cs
+        y1 = (1 + ss) * y + cs * x + ss
+        x2 = (1 + cs) * x + ss * y - ss
+        y2 = (1 + cs) * y - ss * x + cs
+        x3 = (1 - ss) * x + cs * y - cs
+        y3 = (1 - ss) * y - cs * x - ss
+        x4 = (1 - cs) * x - ss * y + ss
+        y4 = (1 - cs) * y + ss * x - cs
+        zs = th.cat((x1, y1, x2, y2, x3, y3, x4, y4), dim=1)
+
+        return z + self.output(zs)
