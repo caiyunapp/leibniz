b4fb0366cbe18e733b1fc5c0ea3fb8995e1f8eb4
mingli <mingli.yuan@gmail.com>
2021-03-15 10:50:21 +0800

    bump to 0.1.16: add hunet

--- leibniz/nn/net/simple.py
+++ leibniz/nn/net/simple.py
@@ -12,8 +12,9 @@ class Identity(nn.Module):
 class Linear(nn.Module):
     def __init__(self, in_channels, out_channels, **kwargs):
         super(Linear, self).__init__()
-        kwargs = dict(kernel_size=3, padding=1).update(kwargs)
-        self.conv = nn.Conv2d(in_channels, out_channels, **kwargs)
+        params = dict(kernel_size=3, padding=1)
+        params.update(kwargs)
+        self.conv = nn.Conv2d(in_channels, out_channels, **params)
 
     def forward(self, x):
         return self.conv(x)
