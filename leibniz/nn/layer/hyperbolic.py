0107007069e7a34e457a2459d3238876c0e828da
mingli <mingli.yuan@gmail.com>
2021-03-25 15:42:13 +0800

    bump to 0.1.29: change bottleneck

--- leibniz/nn/layer/hyperbolic.py
+++ leibniz/nn/layer/hyperbolic.py
@@ -46,52 +46,48 @@ class Bottleneck(nn.Module):
 
 
 class HyperBasic(nn.Module):
-    extension = 3
+    extension = 1
     least_required_dim = 1
 
     def __init__(self, dim, step, relu, conv, reduction=16):
         super(HyperBasic, self).__init__()
-        self.dim = dim // 3
+        self.dim = dim
         self.step = step
-        self.output = BasicBlock(7 * self.dim, 3 * self.dim, step, relu, conv, reduction=reduction)
+        self.input = BasicBlock(dim, 2 * dim, step, relu, conv, reduction=reduction)
+        self.output = BasicBlock(7 * self.dim, self.dim, step, relu, conv, reduction=reduction)
 
-    def forward(self, xs):
-        x, cs, ss = xs[:, :self.dim], xs[:, self.dim:self.dim*2], xs[:, self.dim*2:]
+    def forward(self, x):
+        input = self.input(x)
+        cs, ss = input[:, :self.dim] * self.step, input[:, self.dim:] * self.step
 
         y1 = (1 + ss) * x + cs
         y2 = (1 + cs) * x - ss
         y3 = (1 - cs) * x + ss
         y4 = (1 - ss) * x - cs
+
         ys = th.cat((y1, y2, y3, y4, cs, ss, x), dim=1)
-
-        out = self.output(ys)
-        dx, dcs, dss = out[:, :self.dim], out[:, self.dim:self.dim*2], out[:, self.dim*2:]
-        x, cs, ss = x + dx, cs + dcs, ss + dss
-
-        return th.cat((x, cs, ss), dim=1)
+        return self.output(ys)
 
 
 class HyperBottleneck(nn.Module):
-    extension = 3
+    extension = 4
     least_required_dim = 1
 
     def __init__(self, dim, step, relu, conv, reduction=16):
         super(HyperBottleneck, self).__init__()
-        self.dim = dim // 3
+        self.dim = dim
         self.step = step
-        self.output = Bottleneck(7 * self.dim, 3 * self.dim, step, relu, conv, reduction=reduction)
+        self.input = Bottleneck(dim, 2 * dim, step, relu, conv, reduction=reduction)
+        self.output = Bottleneck(7 * dim, dim, step, relu, conv, reduction=reduction)
 
-    def forward(self, xs):
-        x, cs, ss = xs[:, :self.dim], xs[:, self.dim:self.dim*2], xs[:, self.dim*2:]
+    def forward(self, x):
+        input = self.input(x)
+        cs, ss = input[:, :self.dim] * self.step, input[:, self.dim:] * self.step
 
         y1 = (1 + ss) * x + cs
         y2 = (1 + cs) * x - ss
         y3 = (1 - cs) * x + ss
         y4 = (1 - ss) * x - cs
+
         ys = th.cat((y1, y2, y3, y4, cs, ss, x), dim=1)
-
-        out = self.output(ys)
-        dx, dcs, dss = out[:, :self.dim], out[:, self.dim:self.dim*2], out[:, self.dim*2:]
-        x, cs, ss = x + dx, cs + dcs, ss + dss
-
-        return th.cat((x, cs, ss), dim=1)
+        return self.output(ys)
